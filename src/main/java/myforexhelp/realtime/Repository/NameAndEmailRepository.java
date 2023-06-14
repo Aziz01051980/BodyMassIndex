0635c3f65fbc6427d181b405c7f9c71bd2aaf5e1
Aziz Khalilov <aziz.khalilov1980@gmail.com>
2023-06-14 19:27:53 +0400

    Bodu mass index calculation for JS

--- src/main/java/myforexhelp/realtime/Repository/NameAndEmailRepository.java
+++
@@ -1,9 +0,0 @@
-package myforexhelp.realtime.Repository;
-
-import myforexhelp.realtime.Domain.NameAndEmail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NameAndEmailRepository extends JpaRepository<NameAndEmail, Long> {
-}
