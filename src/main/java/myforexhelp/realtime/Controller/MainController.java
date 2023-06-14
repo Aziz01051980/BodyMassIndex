277b866e90f72bc834931c0cbd3be33c05b5cfa6
Aziz Khalilov <aziz.khalilov1980@gmail.com>
2023-06-14 19:16:39 +0400

    calculator for BMI

---
+++ src/main/java/myforexhelp/realtime/Controller/MainController.java
@@ -0,0 +1,16 @@
+package myforexhelp.realtime.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+
+    @RequestMapping(value = "/bmiIndex", method = RequestMethod.GET)
+    public String getCalculator(){
+        return "bmiIndex";
+    }
+}
