0c3f2352abce1ba25a032c7323179e33f23c3cb2
Aziz Khalilov <aziz.khalilov1980@gmail.com>
2023-06-15 18:05:12 +0400

    adding some scenarios for tests

--- src/main/test/java/health/bmi/MainControllerTests.java
+++ src/main/test/java/health/bmi/MainControllerTests.java
@@ -15,7 +15,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-//@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = MainController.class)
 @AutoConfigureMockMvc
 class MainControllerTests {
